9c7fc70b29b83b791a97440f34689e47686e509c
Joon_Thinkpad <mjypark1212@gmail.com>
2023-09-18 10:38:13 +0800

    wip

--- playground.py
+++ playground.py
@@ -2,3 +2,13 @@ class C:
     @classmethod
     def increment(cls):
         cls.counter += 1
+
+
+def greater(a, b):
+    return a > b
+
+
+nums1 = [1, 2, 3, 4]
+nums2 = [5, 6, 7, 8]
+
+print(list(map(greater, nums1, nums2)))
