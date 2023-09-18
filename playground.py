9308f8f1912d8ef50f843350ac0b75e9fbca36ab
Joon_Thinkpad <mjypark1212@gmail.com>
2023-09-18 10:43:15 +0800

    wip

--- playground.py
+++ playground.py
@@ -12,3 +12,20 @@ nums1 = [1, 2, 3, 4]
 nums2 = [5, 6, 7, 8]
 
 print(list(map(greater, nums1, nums2)))
+
+
+"""
+Kernel mode.
+load code from persistant memory,
+allocate stack with argc/argv
+allocate heap
+call main()
+
+User mode.
+starts program
+ends
+
+Kernel mode.
+Free memory
+remove from process list.
+"""
