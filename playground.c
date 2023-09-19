a1e2f66cbda3e74dafd7ac4a3305f0dabb72d467
Joon_Thinkpad <mjypark1212@gmail.com>
2023-09-19 09:57:28 +0800

    wip

--- playground.c
+++ playground.c
@@ -1,8 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <pthread.h>
-#include <semaphore.h>
 #include <unistd.h>
+#include <semaphore.h>
+#include <pthread.h>
 
 #define NUM_THREADS 4
 #define NUM_ITERATIONS 1000000
@@ -11,7 +11,7 @@ int counter = 0;
 
 sem_t mutex;
 
-void *count(void *arg){
+void *count(void *argv) {
   for (int i = 0; i < NUM_ITERATIONS; i++){
     sem_wait(&mutex);
     counter++;
@@ -20,7 +20,6 @@ void *count(void *arg){
 
   return NULL;
 }
-
 
 int main(int argc, char *argv []){
   pthread_t threads[NUM_THREADS];
@@ -37,8 +36,8 @@ int main(int argc, char *argv []){
 
   sem_destroy(&mutex);
 
-  printf("Final counter value: %d\n", counter);
-  printf("Expected value: %d\n", NUM_ITERATIONS * NUM_THREADS);
+  printf("counter: %d\n", counter);
+  printf("expected: %d\n", NUM_ITERATIONS * NUM_THREADS);
 
   return 0;
 }
