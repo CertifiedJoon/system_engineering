32d17ffe826208c4ba4be6580c0c1349ec3b789d
Joon_Thinkpad <mjypark1212@gmail.com>
2023-09-22 07:43:44 +0800

    wip

--- playground.py
+++ playground.py
@@ -8,3 +8,54 @@ print(dict1.pop(0, "Z"))
 print(dict1.popitem())
 dict1.update(dict({4: "D"}))
 print(dict1 | dict2)
+
+"""
+OS 
+loads code
+allocate stack/heap
+execute main
+
+Hardware
+Runs program
+
+Process
+executes code
+calls a system call
+
+Hardware
+Trap
+
+OS
+execute system call
+returnfromtrap
+
+Hardware
+return execution
+
+
+
+
+Os boots
+create trap tables
+strat interrupt timer
+
+Hardware boots
+remember the addresses of interrupt handlers
+
+process
+executes
+
+hardware
+timer interrupt and trap
+
+OS
+perform context switch()
+return from trap
+
+hardware
+execute new process
+
+process
+another process runs
+
+"""
