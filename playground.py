6cd3acc89a3edee7575b8a51ccb1dddffdebe448
Joon_Thinkpad <mjypark1212@gmail.com>
2023-09-22 14:48:06 +0800

    wip

--- playground.py
+++ playground.py
@@ -1,61 +1,32 @@
-dict1 = dict({1: "A", 2: "B", 3: "C"})
+import socket
+import sys
 
-dict2 = dict({5: "E"})
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("Socket successfully created")
+except socket.error as e:
+    print("Socket creation failed with error ", str(e))
 
-print(dict1.get(1, "Z"))
-print(dict1.get(0, "Z"))
-print(dict1.pop(0, "Z"))
-print(dict1.popitem())
-dict1.update(dict({4: "D"}))
-print(dict1 | dict2)
+port = 80
 
-"""
-OS 
-loads code
-allocate stack/heap
-execute main
+try:
+    host_ip = socket.gethostbyname("www.google.com")
+except socket.gaierror:
+    print("Error resolving the host")
+    sys.exit()
 
-Hardware
-Runs program
+s.connect((host_ip, port))
 
-Process
-executes code
-calls a system call
+print("the socket has succesffully connected to google")
+ip = ""
 
-Hardware
-Trap
+serverPort = 12000
+serverSocket = socket.socket(AF_INET, SOCK_DGRAM)
+serverSocket.bind(("", serverPort))
 
-OS
-execute system call
-returnfromtrap
+serverSocket.listen(5)
+print("server is ready to receive")
 
-Hardware
-return execution
-
-
-
-
-Os boots
-create trap tables
-strat interrupt timer
-
-Hardware boots
-remember the addresses of interrupt handlers
-
-process
-executes
-
-hardware
-timer interrupt and trap
-
-OS
-perform context switch()
-return from trap
-
-hardware
-execute new process
-
-process
-another process runs
-
-"""
+while True:
+    message, clientAddres = serverSocket.recvfrom(2048)
+    print(message.upper())
