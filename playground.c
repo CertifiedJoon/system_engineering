6d93ca43c4fece849281a1bac58d4f015cbc8c89
Joon_Thinkpad <mjypark1212@gmail.com>
2023-09-26 08:34:17 +0800

    wip

--- playground.c
+++ playground.c
@@ -1,39 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <semaphore.h>
-#include <pthread.h>
+#include <unistd.h>
 
-#define NUM_THREADS 4
-#define NUM_ITER 1000000
-
-sem_t mutex;
-int counter = 0;
-
-void *count(){
-  for (int i = 0; i < NUM_ITER; i++){
-    sem_wait(&mutex);
-    counter++;
-    sem_post(&mutex);
+int main(int argc, char *argv []){
+  int pid = fork();
+  
+  if (pid == 0) {
+    // child process
+    printf("Hello World\n");
+  } else {
+    fork();
+    printf("Hello World\n");
   }
-  return NULL;
-}
-
-int main(int argc, char *argv []) {
-  pthread_t threads[NUM_THREADS];
-  sem_init(&mutex, 0, 1);
-
-  for (int i = 0; i < NUM_THREADS; i++){
-    pthread_create(&threads[i], NULL, count, NULL);
-  }
-
-  for (int i = 0; i < NUM_THREADS; i++) {
-    pthread_join(threads[i], NULL);
-  }
-
-  sem_destroy(&mutex);
-
-  printf("Counted: %d\n", counter);
-  printf("Expected: %d\n", NUM_ITER * NUM_THREADS);
 
   return 0;
 }
