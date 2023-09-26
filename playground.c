b91b2d61aa0f35b22745a386ba9170aba2ef4486
Joon_Thinkpad <mjypark1212@gmail.com>
2023-09-26 08:40:58 +0800

    wip

--- playground.c
+++ playground.c
@@ -1,17 +1,40 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
+#include <pthread.h>
+#include <semaphore.h>
 
-int main(int argc, char *argv []){
-  int pid = fork();
-  
-  if (pid == 0) {
-    // child process
-    printf("Hello World\n");
-  } else {
-    fork();
-    printf("Hello World\n");
+#define NUM_THREADS 4
+#define NUM_COUNT 1000000
+
+sem_t mutex;
+int counter = 0;
+
+void *count() {
+  for (int i = 0; i < NUM_COUNT; i++) {
+    sem_wait(&mutex);
+    counter++;
+    sem_post(&mutex);
   }
+
+  return NULL;
+}
+
+int main(int argc, char *argv[]) {
+  pthread_t threads[NUM_THREADS];
+
+  sem_init(&mutex, 0, 1);
+  
+  for (int i = 0; i < NUM_THREADS; i++){
+    pthread_create(&threads[i], NULL, count, NULL);
+  }
+
+  for (int i = 0; i < NUM_THREADS; i++){
+    pthread_join(threads[i], NULL);
+  }
+
+  sem_destroy(&mutex);
+
+  printf("%d\n", counter);
 
   return 0;
 }
