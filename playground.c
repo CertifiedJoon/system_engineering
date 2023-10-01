294e9427ace16858f286f832f5d47019536a0f69
Joon_Thinkpad <mjypark1212@gmail.com>
2023-10-01 23:25:44 +0800

    wip

--- playground.c
+++ playground.c
@@ -1,22 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+
+void sig10Handler(int sig_num){
+  exit();
+}
+
 
 int main(int argc, char *argv[]){
-  if (argc != 2) {
-    return -1;
+  signal(10, sig10Handler);
+
+  while(true){
+    sleep(1);
+    printf("still running");
   }
-
-  int *arr = (int*)malloc(sizeof(int) * atoi(argv[1]));
-
-  for (int i = 0; i < atoi(argv[1]); i++){
-    arr[i] = i * i;
-  }
-
-  for (int i = 0; i < atoi(argv[1]); i++){
-    printf("%d\t", arr[i]);
-  }
-
-  printf("\n");
 
   return 0;
 }
