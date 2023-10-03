abe102697cd539045257aa9b7886810ebad38545
Joon_Thinkpad <mjypark1212@gmail.com>
2023-10-03 21:18:08 +0800

    wip

---
+++ playground.py
@@ -0,0 +1,47 @@
+"""
+incremental delivery
+
+1. specification + assign requirements to incrementds
+2. system design
+4. component design
+5. component dev
+6. component validate + integrate
+7. component deploy
+8. loop to 4
+
+adv:
+1. trust between client and dev
+2. less likely to fail project due to constant feedback
+3. the core components naturally get more testing
+
+disadv:
+1. conflicts with procurement model
+2. some components are interconnected so they cannot be delivered
+incrementally
+
+
+Waterfall
+1. project specification
+2. product design
+3. implementation and unit testing
+4. integration and system testing
+5. operation and maintenance.
+
+adv:
+1. great to coordinate work with cross-regional teams
+2. great for complex system with complex system specification
+3. great for documentation
+
+disadv:
+1. inflexible to change
+2. insufficient project specification is catastrophic
+
+Benefits of scrum
+1. involvement of customer in the loop
+2. flexible to change
+3. communication
+4. iterative approach helps process maturation
+5. incremental delivery
+6. product is broken down to understandable chunks
+
+"""
