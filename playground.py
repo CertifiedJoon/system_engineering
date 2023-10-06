6a967180a755b8cf3ac7fb0acc36110037f82930
Joon_Thinkpad <mjypark1212@gmail.com>
2023-10-06 09:57:20 +0800

    wip

--- playground.py
+++ playground.py
@@ -1,61 +1,19 @@
-"""
-incremental delivery
+import heapq
 
-1. specification + assign requirements to incrementds
-2. system design
-4. component design
-5. component dev
-6. component validate + integrate
-7. component deploy
-8. loop to 4
+arr = [1, 2, 4, 5, 3, 6, 2, 3, 1]
 
-adv:
-1. trust between client and dev
-2. less likely to fail project due to constant feedback
-3. the core components naturally get more testing
+heapq.heapify(arr)
 
-disadv:
-1. conflicts with procurement model
-2. some components are interconnected so they cannot be delivered
-incrementally
+print(arr)
 
+heapq.heappush(arr, 1)
 
-Waterfall
-1. project specification
-2. product design
-3. implementation and unit testing
-4. integration and system testing
-5. operation and maintenance.
+print(arr)
 
-adv:
-1. great to coordinate work with cross-regional teams
-2. great for complex system with complex system specification
-3. great for documentation
+a = heapq.heappop(arr)
 
-disadv:
-1. inflexible to change
-2. insufficient project specification is catastrophic
+print(a)
 
-Benefits of scrum
-1. involvement of customer in the loop
-2. flexible to change
-3. communication
-4. iterative approach helps process maturation
-5. incremental delivery
-6. product is broken down to understandable chunks
+print(heapq.nlargest(3, arr))
 
-extremem programming is a agile practice
-pair programming
-collective ownership
-customer involvement
-test driven development
-incremental delivery
-
-people over process
-customer involvement
-flexible to change
-
-
-
-
-"""
+print(heapq.nsmallest(4, arr))
