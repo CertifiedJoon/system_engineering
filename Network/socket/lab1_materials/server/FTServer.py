2e0568e3667e8f6fb0261f15fcf84780b5e42ce5
Joon_Thinkpad <mjypark1212@gmail.com>
2023-10-06 16:18:18 +0800

    wip

--- Network/socket/lab1_materials/server/FTServer.py
+++ Network/socket/lab1_materials/server/FTServer.py
@@ -9,42 +9,69 @@ def main(argv):
     port = argv[1]
 
     # create socket and bind
-    sockfd = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-    sockfd.bind(("127.0.0.1", int(port)))
+
+    try:
+        sockfd = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+        sockfd.bind(("127.0.0.1", int(port)))
+    except socket.error as e:
+        print(str(e))
+        sys.exit(1)
 
     sockfd.listen(5)
 
     print("The server is ready to receive")
 
     while True:
-        # accept new connection
-        conn, addr = sockfd.accept()
+        try:
+            # accept new connection
+            try:
+                conn, addr = sockfd.accept()
+                print(f"Connection Established. Here is remote peer info: {addr}")
+            except Exception as e:
+                raise RuntimeError(e)
 
-        # receive file name/size message from client
-        msg = conn.recv(1000).decode("ascii")
+            # receive file name/size message from client
+            try:
+                msg = conn.recv(1024).decode("ascii")
+            except Exception as e:
+                raise RuntimeError(e)
 
-        # use Python string split function to retrieve file name and file size from the received message
-        fname, filesize = msg.split(":")
+            # use Python string split function to retrieve file name and file size from the received message
+            fname, filesize = msg.split(":")
 
-        print("Open a file with name '%s' with size %s bytes" % (fname, filesize))
+            print("Open a file with name '%s' with size %s bytes" % (fname, filesize))
 
-        # create a new file with fname
-        fd = open(fname, "w")
+            # create a new file with fname
+            try:
+                fd = open(fname, "wb")
+            except OSError:
+                raise RuntimeError("File cannot be located")
 
-        remaining = int(filesize)
+            remaining = int(filesize)
 
-        conn.send(b"OK")
+            conn.send(b"OK")
 
-        print("Start receiving . . .")
-        while remaining > 0:
-            # receive the file content into rmsg and write into the file
-            rmsg = conn.recv(1000).decode("ascii")
-            fd.write(rmsg)
-            remaining -= len(rmsg)
+            print("Start receiving . . .")
+            while remaining > 0:
+                # receive the file content into brmsg and write into the file
+                try:
+                    rmsg = conn.recv(1024)
+                except socket.error:
+                    raise RuntimeError("Exception has occured")
 
-        print("[Completed]")
-        fd.close()
-        conn.close()
+                try:
+                    fd.write(rmsg)
+                    remaining -= len(rmsg)
+                except OSError:
+                    raise RuntimeError("Cannot write")
+
+            print("[Completed]")
+            fd.close()
+            conn.close()
+
+        except RuntimeError as e:
+            print(e)
+            continue
 
     sockfd.close()
 
