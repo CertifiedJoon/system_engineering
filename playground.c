9997aab03e7db8878757e98480b282cb8b937caf
Joon_Thinkpad <mjypark1212@gmail.com>
2023-10-26 07:48:49 +0800

    wip

--- playground.c
+++ playground.c
@@ -1,21 +1,2 @@
-typedef struct __lock_t {
-  int flag;
-} lock_t;
-
-int linkedloaded(lock_t *lock){
-  return lock->flag;
-}
-
-int storeConditional(int* ptr, int value) {
-  if (/*No change to linked address*/){
-    *ptr = value;
-    return 1;
-  } else {
-    return 0;
-  }
-  
-}
-
-void lock (lock_t *lock) {
-  while (LoadLinked(&lock->flag) || !StoreConditional(&lock->flag, 1));
-}
+pthread_create(&thread, NULL, mythread, (void *)(100))
+pthread_join(thread, (void **)rval)
