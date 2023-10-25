3f3bc7d425d093eec1819ae3a61b5985d7753820
Joon_Thinkpad <mjypark1212@gmail.com>
2023-10-26 07:58:51 +0800

    wip

--- playground.c
+++ playground.c
@@ -1,2 +1,13 @@
-pthread_create(&thread, NULL, mythread, (void *)(100))
-pthread_join(thread, (void **)rval)
+typedef struct __lock_t {
+  int turn;
+  int ticket;
+} lock_t;
+
+int fetchTicket(lock_t *lock){
+  return (lock->ticket)++;
+}
+
+int isCorrectTurn(lock_t *lock, int ticket) {
+  return lock->turn == ticket;
+}
+
