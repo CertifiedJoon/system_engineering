7605d132d60f065eca4be914c855184db05a367f
Joon_Thinkpad <mjypark1212@gmail.com>
2023-11-03 11:38:17 +0800

    wip

--- playground.py
+++ playground.py
@@ -1,34 +1,13 @@
-from time import sleep
+arr = [i for i in range(10)]
 
+del arr[1:2]
 
-def factor(n):
-    for i in range(1, 1 + n):
-        if n % i == 0:
-            yield i
+del arr[1:3:2]
 
+del arr
 
-for i in factor(4):
-    sleep(1)
-    print(i)
+arr2 = arr
 
-
-class LinkedList:
-    def __init__(self):
-        self._val = None
-        self._next = None
-
-    def iter(self):
-        return self._next
-
-
-def reverse(head):
-    curr = head
-    prev = None
-
-    while curr:
-        next = curr.next
-        curr.next = prev
-        prev = curr
-        curr = next
-
-    return prev
+arr2.insert(1, 2)
+arr2.remove(7)
+print(reversed(arr))
