c641c88aced4b86f69e2816141090e435db7c5e5
Joon_Thinkpad <mjypark1212@gmail.com>
2023-11-03 14:43:19 +0800

    wip

--- playground.c
+++ playground.c
@@ -1,52 +1,32 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <pthread.h>
-#include <signal.h>
+#include <time.h>
+#include <sys/wait.h>
 
-
-int main(int argc, char *argv[]) {
-  int fd[2];
-  if (pipe(fd) == -2) return 1;
+int main(int argc, char *argv[]){
   int pid = fork();
-  if (pid == -1) return 2;
 
+  if (pid == -1) {return 1;}
   if (pid == 0) {
-    int n, i;
-    srand(time(NULL));
-    n = rand() % 10 + 1;
-    int *arr = (int *) malloc (sizeof(int) * n);
-
-    for (i = 0; i < n; i++) {
-      arr[i] = i;
-      printf("%d ", arr[i]);
-    }
-    printf("\n");
-    
-    close(fd[0]);
-    if (write(fd[1], &n, sizeof(int)) == -1) return 3;
-    printf("wrote size from child\n");
-    if (write(fd[1], arr, sizeof(int) * n) == -1) return 4;
-    printf("wrote array from child\n");
-    close(fd[1]);
+    // child process
+    FILE *fp = fopen("smt.txt");
+    dup2(fp, STDOUT);
+    execlp("ping", "ping", "-c", "3", "google.com", NULL);
   } else {
-    int n, i;
-
-    close(fd[1]);
-    if (read(fd[0], &n, sizeof(int)) == -1) return 5;
-    printf("read size %d\n", n);
-    int *arr = (int*) malloc(sizeof(int) * n);
-    if (read(fd[0], arr, sizeof(int) * n) == -1) return 6;
-    printf("Received Array \n");
-    close(fd[0]);
-
-    int sum = 0;
-    for (i = 0; i < n; i++) {
-      sum +=  arr[i];
+    // parent process
+    int wstatus;
+    wait(&wstatus);
+    if (WIFEXITED(wstatus)) {
+      int statusCode = WEXITSTATUS(wstatus);
+      if (statusCode == 0) {
+        printf("Success!\n");
+      } else {
+        printf("Failure! Status Code %d\n", statusCode);
+      }
     }
-    printf("%d\n", sum);
-    wait(NULL);
-  }
-
+    printf("Some post processing goes here!\n");
+  } 
+  
   return 0;
 }
