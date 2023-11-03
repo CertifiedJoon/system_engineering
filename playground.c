f6f563d9ad025cf74bc00fb303a05c1ac78a9bc7
Joon_Thinkpad <mjypark1212@gmail.com>
2023-11-03 14:11:14 +0800

    wip

--- playground.c
+++ playground.c
@@ -1,33 +1,35 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
-#include <time.h>
-#include <sys/wait.h>
+#include <pthread.h>
+#include <semaphore.h>
 
-int main(int argc, char *argv[]){
-  int pid = fork();
+#define SERVER_CAP = 12
 
-  if (pid == -1) {return 1;}
-  if (pid == 0) {
-    // child process
-    FILE* fp = fopen("output.txt", O_WRONLY | O_CREAT, 0777);
-    dup2 (fp, STDOUT);
-    close(fp);
-    execlp("ping", "ping", "-c", "3", "google.com", NULL);
-  } else {
-    // parent process
-    int wstatus;
-    wait(&wstatus);
-    if (WIFEXITED(wstatus)) {
-      int statusCode = WEXITSTATUS(wstatus);
-      if (statusCode == 0) {
-        printf("Success!\n");
-      } else {
-        printf("Failure! Status Code %d\n", statusCode);
-      }
-    }
-    printf("Some post processing goes here!\n");
-  } 
-  
+sem_t logged;
+
+void* serverWelcome(void* args) {
+  sem_wait(&logged);
+  sleep(2);
+  printf("Logged in %d\n", *(int *) args);
+  sem_post(&logged);
+  printf("Logged out %d\n", *(int *) args);
+}
+
+int main(int argc, char *argv[]) {
+  sem_init(&logged, 0, 12);
+  pthread_t threads[20];
+
+  for (int i = 0; i < 20; i++) {
+    int *a = malloc(sizeof(int));
+    *a = i;
+    pthread_create(&threads[i], NULL, serverWelcome, a);
+  }
+
+  for (int i = 0; i < 20; i++) {
+    pthread_join(threads[i], NULL);
+  }
+
+  sem_destroy(&logged);
   return 0;
 }
