e2c358ba21ff9fdcf3f48b886358cc7cbd4df0db
Joon_Thinkpad <mjypark1212@gmail.com>
2023-11-03 14:19:26 +0800

    wip

--- playground.c
+++ playground.c
@@ -1,35 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <semaphore.h>
-
-#define SERVER_CAP = 12
-
-sem_t logged;
-
-void* serverWelcome(void* args) {
-  sem_wait(&logged);
-  sleep(2);
-  printf("Logged in %d\n", *(int *) args);
-  sem_post(&logged);
-  printf("Logged out %d\n", *(int *) args);
-}
-
-int main(int argc, char *argv[]) {
-  sem_init(&logged, 0, 12);
-  pthread_t threads[20];
-
-  for (int i = 0; i < 20; i++) {
-    int *a = malloc(sizeof(int));
-    *a = i;
-    pthread_create(&threads[i], NULL, serverWelcome, a);
-  }
-
-  for (int i = 0; i < 20; i++) {
-    pthread_join(threads[i], NULL);
-  }
-
-  sem_destroy(&logged);
-  return 0;
-}
+/**
+ * Process is created by 
+ * 1. load code from hard disk drive
+ * 2. allocate stack memory
+ * 4. store pc, pid into pcb and place into process list
+ * 3. allocate heap memory
+ * 
+ * Multi core scheduling
+ * 1. single queue implementation -> cache affinity poor scalability
+ * 2. multi queue implementation -> provides cache affinity. less concurrency issue. but load imbalance, solved by work stealing
+ *  
+ * pthread_create(&thread, NULL, routine, * value)
+ * 
+ * pthread_join(thread, **rvalue)
+ */ 
