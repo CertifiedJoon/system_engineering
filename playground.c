def06736de5dc927c5fa5b50211a095e3d8a9ce3
Joon_Thinkpad <mjypark1212@gmail.com>
2023-11-03 14:26:29 +0800

    wip

--- playground.c
+++ playground.c
@@ -1,15 +1,18 @@
-/**
- * Process is created by 
- * 1. load code from hard disk drive
- * 2. allocate stack memory
- * 4. store pc, pid into pcb and place into process list
- * 3. allocate heap memory
- * 
- * Multi core scheduling
- * 1. single queue implementation -> cache affinity poor scalability
- * 2. multi queue implementation -> provides cache affinity. less concurrency issue. but load imbalance, solved by work stealing
- *  
- * pthread_create(&thread, NULL, routine, * value)
- * 
- * pthread_join(thread, **rvalue)
- */ 
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include <signal.h>
+
+
+int main(int argc, char *argv[]) {
+  int id = fork();  
+  if (id == 0) {
+    while(1) sleep(1);
+  } else {
+    kill(id, SIGKILL);
+    wait(NULL);
+  }
+
+  return 0;
+}
