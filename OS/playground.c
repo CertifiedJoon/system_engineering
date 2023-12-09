8e7259adbcd5cbbc45db811acf6a559ef10959ab
Joon_Thinkpad <mjypark1212@gmail.com>
2023-12-09 16:51:21 +0800

    piping

--- OS/playground.c
+++ OS/playground.c
@@ -1,39 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <time.h>
-int main(int argc, char* argv[]) {
-  int p1[2];
-  int p2[2];
+int loadLinked(int *ptr) {
+  return *ptr;
+}
 
-  pipe(p1);
-  pipe(p2);
-
-  int pid = fork();
-
-  if (pid == 0) {
-    // child process
-    close(p1[1]);
-    close(p2[0]);
-    int x;
-    read(p1[0], &x, sizeof(x));
-    x *= 4;
-    write(p2[1], &x, sizeof(x));
-    close(p1[0]);
-    close(p2[1]);
+int StoreConditional(int *ptr, int value) {
+  if (no update to *ptr since loadlinked) {
+    *ptr  = value;
+    return 1
   } else {
-    // parent process
-    close(p1[0]);
-    close(p2[1]);
-    srand(time(NULL));
-    int y = rand() % 100;
-    write(p1[1], &y, sizeof(y));
-    printf("Wrote %d\n", y);
-    read(p2[0], &y, sizeof(y));
-    printf("Read %d\n", y);
-    close(p1[1]);
-    close(p2[0]);
+    return 0;
   }
+}
 
-  return 0;
+void lock(lock_t *lock) {
+  while (Loadlinked(&lock->flag) || !StoreConditional(&lock->flag, 1))
+  ;
 }
